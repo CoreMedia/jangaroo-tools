c6d1f7f40a37f0ab0faaf4c63ad8840ed60a19af
Dennis Homann <dennis.homann@coremedia.com>
2008-05-27 16:30:05 +0100

    - morphed non-functional workspace into a standardized mvn layout - non-functional build files have been removed - integration tests fail, but the errors seem to be real bugs (or bugs in the test cases) - build currently depends on patched shitty-maven-plugin and a CM private deployment of edu.princeton.cup:java-cup and com.sun.j2se:tools
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 134095]

--- joodoc/jscdoc.sh
+++
@@ -1,6 +0,0 @@
-# check source dir arg
-if [ -z "$1" ]; then
-  echo "usage: jscdoc -d targetdir file1.jsc file2.jsc ..."
-fi
-java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -cp ./output/lib/jscdoc.jar:../thirdparty/sun/j2se/lib/tools.jar:../jscc/output/lib/jscc.jar com/coremedia/tools/jscdoc/Main $1 $2 $3 $4 $5 $6 $7 $8 $9 
-
