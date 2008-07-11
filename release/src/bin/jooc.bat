8a10f9b08e726bc456489bc4619f7b35cbeda34c
Olaf Kummer <olaf.kummer@coremedia.com>
2008-07-11 18:50:35 +0100

    call correct class
    make sure that arguments are passed to Java
    
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 136067]

--- release/src/bin/jooc.bat
+++ release/src/bin/jooc.bat
@@ -8,4 +8,4 @@ set JOO_HOME=%~dp0..
 set JOO_CLASSPATH=%JOO_HOME%\lib\jangaroo-compiler-${joo.tools.version}.jar
 set JOO_CLASSPATH=%JOO_CLASSPATH%;%JOO_HOME%\lib\java-cup-runtime-10k.jar;%JOO_HOME%\lib\commons-cli-1.1.jar
 
-"%~dp0runtool.bat" com.coremedia.jscc.Jscc "%JOO_CLASSPATH%" %*
+"%~dp0runtool.bat" net.jangaroo.jooc.Jooc "%JOO_CLASSPATH%" %*
