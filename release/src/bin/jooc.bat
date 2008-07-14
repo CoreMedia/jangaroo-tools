2d373feb5494fe580fe0e3acd8ed139cddfaa320
Dennis Homann <dennis.homann@coremedia.com>
2008-07-14 15:12:03 +0100

    - changed jooc(.bat) to make use of the manifest classpath - removed runtool(.bat)
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 136107]

--- release/src/bin/jooc.bat
+++ release/src/bin/jooc.bat
@@ -4,8 +4,12 @@ if "%OS%"=="Windows_NT" @setlocal
 if "%OS%"=="WINNT" @setlocal
 
 set JOO_HOME=%~dp0..
+set JAVA_ARGS=-jar "%JOO_HOME%\lib\jangaroo-compiler-${joo.tools.version}.jar"
 
-set JOO_CLASSPATH=%JOO_HOME%\lib\jangaroo-compiler-${joo.tools.version}.jar
-set JOO_CLASSPATH=%JOO_CLASSPATH%;%JOO_HOME%\lib\java-cup-runtime-10k.jar;%JOO_HOME%\lib\commons-cli-1.1.jar
-
-"%~dp0runtool.bat" net.jangaroo.jooc.Jooc "%JOO_CLASSPATH%" %*
+if "%JAVA_HOME%" == "" (
+  echo ERROR: JAVA_HOME environment variable is not set.
+  echo Please set the JAVA_HOME variable in your environment to match the
+  echo location of your Java installation.
+) else (
+  "%JAVA_HOME%\bin\java" %JAVA_ARGS% %*
+)
