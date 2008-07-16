b1f428522d912d9aed7d451e644da1e05d440606
Olaf Kummer <olaf.kummer@coremedia.com>
2008-07-16 16:57:10 +0100

    Apache license 2 let's hope that's it, but next time it will be possible to do an update automatically
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 136263]

--- joodoc/src/main/java/net/jangaroo/joodoc/RootDocImpl.java
+++ joodoc/src/main/java/net/jangaroo/joodoc/RootDocImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008 CoreMedia AG
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language 
+ * governing permissions and limitations under the License.
+ */
+
 // Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
 // Jad home page: http://www.geocities.com/kpdus/jad.html
 // Decompiler options: packfields(3) packimports(7) deadcode fieldsfirst splitstr(64) nonlb lnc radix(10) lradix(10) 
