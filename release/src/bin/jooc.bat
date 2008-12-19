f98164bc30693ba1f58aeb0f2b6f97b9eb3f40c2
Dennis Homann <dennis.homann@coremedia.com>
2008-12-19 15:56:26 +0100

    use variable ${pom.version} b/c ${joo.tools.version} is no longer defined
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 143183]

--- release/src/bin/jooc.bat
+++ release/src/bin/jooc.bat
@@ -4,7 +4,7 @@ if "%OS%"=="Windows_NT" @setlocal
 if "%OS%"=="WINNT" @setlocal
 
 set JOO_HOME=%~dp0..
-set JAVA_ARGS=-jar "%JOO_HOME%\lib\jangaroo-compiler-${joo.tools.version}.jar"
+set JAVA_ARGS=-jar "%JOO_HOME%\lib\jangaroo-compiler-${pom.version}.jar"
 
 if "%JAVA_HOME%" == "" (
   echo ERROR: JAVA_HOME environment variable is not set.
