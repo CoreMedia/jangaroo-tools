0359b39ada3dbb7e5ff29303cae6826c9bcdb5ef
Manuel Ohlendorf <manuel.ohlendorf@coremedia.com>
2009-08-07 15:02:28 +0100

    better test name
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 156656]

--- maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/TestCompilerMojo.java
+++ maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/TestCompilerMojo.java
@@ -45,7 +45,7 @@ public class TestCompilerMojo extends AbstractCompilerMojo {
    * When "mergeOutput" is "true", this parameter specifies the name of the output file containing all
    * compiled classes. Otherwise, this parameter will be ignored.
    *
-   * @parameter expression="${project.build.directory}/joo-test/${project.build.finalName}.js"
+   * @parameter expression="${project.build.directory}/joo-test/${project.build.finalName}-test.js"
    */
   private String testOutputFileName;
 
