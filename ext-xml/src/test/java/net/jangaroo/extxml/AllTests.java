2a621cdd88dd6e0d22e16f9e5fb0209fe6ec835f
Manuel Ohlendorf <manuel.ohlendorf@coremedia.com>
2009-09-11 09:26:12 +0100

    test suite for IDE help
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 159015]

---
+++ ext-xml/src/test/java/net/jangaroo/extxml/AllTests.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2009, CoreMedia AG, Hamburg. All rights reserved.
+ */
+package net.jangaroo.extxml;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ExtComponentSrcFileScannerTest.class,
+    XmlToJsonHandlerTest.class,
+    JooClassGeneratorTest.class,
+    ExtComponentSrcFileScannerTest.class})
+public class AllTests {
+}
