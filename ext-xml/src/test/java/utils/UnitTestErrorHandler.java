75b531bc384d79a48c2648771eb2f690ffcb4151
Manuel Ohlendorf <manuel.ohlendorf@coremedia.com>
2009-09-17 18:04:23 +0100

    unit test error handler
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 159510]

---
+++ ext-xml/src/test/java/utils/UnitTestErrorHandler.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2009, CoreMedia AG, Hamburg. All rights reserved.
+ */
+package utils;
+
+import net.jangaroo.extxml.ErrorHandler;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+/**
+ *
+ */
+public class UnitTestErrorHandler implements ErrorHandler{
+
+  public String expectedErrorMessage;
+  private boolean expectedErrorOccured;
+
+  public void setCurrentFile(File file) {
+
+  }
+
+
+  public void error(String message, int lineNumber, int columnNumber) {
+    fail(message);
+  }
+
+
+  public void error(String message, Exception exception) {
+    fail(message);
+  }
+
+
+  public void error(String message) {
+    if(expectedErrorMessage != null) {
+      assertEquals(expectedErrorMessage, message);
+      expectedErrorOccured = true;
+    } else {
+      fail(message);
+    }
+  }
+
+
+  public void warning(String message) {
+
+  }
+
+
+  public void warning(String message, int lineNumber, int columnNumber) {
+
+  }
+
+  public void info(String message) {
+
+  }
+
+  public void checkExpectedErrors() {
+    if(!expectedErrorOccured && expectedErrorMessage != null) {
+      fail(String.format("expected error %s not occured", expectedErrorMessage));
+    }
+  }
+}
