04bd6de346faa7fea902255f4d2d69e0a78b0809
Manuel Ohlendorf <manuel.ohlendorf@coremedia.com>
2009-11-20 16:55:03 +0100

    style
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 164411]

--- jangaroo-core/jangaroo-compiler/src/main/java/net/jangaroo/jooc/Debug.java
+++ jangaroo-core/jangaroo-compiler/src/main/java/net/jangaroo/jooc/Debug.java
@@ -17,18 +17,18 @@ package net.jangaroo.jooc;
 
 /**
  * A class to provide debug facilities in java.
- *
+ * <p/>
  * For <CODE>assertTrue</CODE>, to avoid evaluation of the boolean expression with
  * debugging turned off on sun's java interpreter and with sun's javac use the
  * following:
- *
+ * <p/>
  * <pre>
  *     Debug.assertTrue(Debug.enabled && anExpression, "error message");
  * </pre>
- *
+ * <p/>
  * The short-cut semantics of '&&' will guarantee that anExpression will not be
  * evaluated if enabled is set to false.
- *
+ * <p/>
  * <CODE>Debug.print</CODE> also won't print its argument if <CODE>Debug.enabled</CODE> is false. If
  * the generation of the argument is already costly, you have to wrap it in an if-statement.
  *
@@ -38,18 +38,24 @@ public class Debug {
 
   public static boolean enabled = true;
 
+  private Debug() {
+    //hide public constructor
+  }
+
   public static final void assertTrue(boolean b, String s) {
-    if (enabled && !b)
+    if (enabled && !b) {
       throw new RuntimeException("Assertion failed: " + s);
+    }
   }
 
   /**
    * Method to use to assert that an array is sorted
    */
-  public static boolean isSorted(int[] array){
-    for(int i = 1; i < array.length; i++){
-      if(array[i] < array[i-1])
+  public static boolean isSorted(int[] array) {
+    for (int i = 1; i < array.length; i++) {
+      if (array[i] < array[i - 1]) {
         return false;
+      }
     }
     return true;
   }
@@ -59,9 +65,11 @@ public class Debug {
    * Uses ==
    */
   public static boolean isInArray(Object[] array, Object find) {
-    for(int i=0; i < array.length; i++)
-      if(array[i] == find)
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == find) {
         return true;
+      }
+    }
     return false;
   }
 
@@ -70,9 +78,11 @@ public class Debug {
    * Uses equals.
    */
   public static boolean isInArray(String[] array, String find) {
-    for(int i=0; i < array.length; i++)
-      if(array[i].equals(find))
+    for (int i = 0; i < array.length; i++) {
+      if (array[i].equals(find)) {
         return true;
+      }
+    }
     return false;
   }
 
@@ -80,65 +90,87 @@ public class Debug {
    * Method to use to assert that an array contains an int.
    */
   public static boolean isInArray(int[] array, int find) {
-    for(int i=0; i < array.length; i++)
-      if(array[i] == find)
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == find) {
         return true;
+      }
+    }
     return false;
   }
 
-  /** Print a String, and then finish the line. */
+  /**
+   * Print a String, and then finish the line.
+   */
   public static final void println(String x) {
-    if (enabled) System.out.println(x);
+    if (enabled) {
+      System.out.println(x);
+    }
   }
 
-  /** Print an Object, and then finish the line. */
+  /**
+   * Print an Object, and then finish the line.
+   */
   public static final void println(Object x) {
-    if (enabled) System.out.println(x);
+    if (enabled) {
+      System.out.println(x);
+    }
   }
 
-  /** Print an Array. */
+  /**
+   * Print an Array.
+   */
   public static final void printArray(Object[] x) {
     if (enabled) {
       System.out.print(printArrayToString(x));
     }
   }
 
-  /** Print an Object Array to a String. */
+  /**
+   * Print an Object Array to a String.
+   */
   public static final String printArrayToString(Object[] x) {
-    if(x == null)
+    if (x == null) {
       return "null";
+    }
 
     StringBuffer buffer = new StringBuffer();
     buffer.append('{');
-    for(int i = 0; i < x.length; i++) {
-      if (i > 0)
+    for (int i = 0; i < x.length; i++) {
+      if (i > 0) {
         buffer.append(',');
+      }
       buffer.append(x[i]);
     }
     buffer.append('}');
     return buffer.toString();
   }
 
-  /** Print an int Array to a String. */
+  /**
+   * Print an int Array to a String.
+   */
   public static final String printArrayToString(int[] x) {
     StringBuffer buffer = new StringBuffer();
     buffer.append('{');
-    for(int i = 0; i < x.length; i++) {
-      if (i > 0)
+    for (int i = 0; i < x.length; i++) {
+      if (i > 0) {
         buffer.append(',');
+      }
       buffer.append(x[i]);
     }
     buffer.append('}');
     return buffer.toString();
   }
 
-  /** Print an Array. */
+  /**
+   * Print an Array.
+   */
   public static final void printArray(int[] x) {
     if (enabled) {
       System.out.print('{');
-      for(int i = 0; i < x.length; i++){
-        if (i > 0)
+      for (int i = 0; i < x.length; i++) {
+        if (i > 0) {
           System.out.print(',');
+        }
         System.out.print(x[i]);
       }
       System.out.println('}');
@@ -148,8 +180,8 @@ public class Debug {
   /**
    * prints stack trace where you are
    */
-  public static void printStackTrace(){
-    try{
+  public static void printStackTrace() {
+    try {
       throw new Exception();
     } catch (Exception e) {
       e.printStackTrace();
