bcf1b5b2ed227c6e5738c559aedecc85314a60d9
Frank Wienberg <frank.wienberg@coremedia.com>
2009-12-09 10:48:37 +0100

    Enabled mohlendo's XsdScanner2, almost works, Manuel takes over.
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 165937]

--- jangaroo-core/ext-xml/src/main/java/net/jangaroo/extxml/XsdScanner.java
+++ jangaroo-core/ext-xml/src/main/java/net/jangaroo/extxml/XsdScanner.java
@@ -1,71 +1,155 @@
+/*
+ * Copyright (c) 2009, CoreMedia AG, Hamburg. All rights reserved.
+ */
 package net.jangaroo.extxml;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.codehaus.plexus.util.StringUtils;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Stack;
 
-/**
- * An XsdScanner parses an Ext XML component declaration schema into a {@link ComponentSuite}.
- */
-public final class XsdScanner {
+public class XsdScanner {
+
   private static final String XML_SCHEMA_URL = "http://www.w3.org/2001/XMLSchema";
 
-  private DocumentBuilder builder = null;
+  private ComponentSuite componentSuite;
+  private XMLStreamReader parser = null;
+  private Stack<ComponentClass> ccStack = new Stack<ComponentClass>();
+  private boolean isInsideComplexType = false;
+  private boolean isInsideCfg = false;
+  private static final String SCHEMA = "schema";
+  private static final String COMPLEX_TYPE = "complexType";
+  private static final String ELEMENT = "element";
+  private static final String ATTRIBUTE = "attribute";
+  private static final String EXTENSION = "extension";
 
-  public XsdScanner() {
-    DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-    builderFactory.setNamespaceAware(true);
-    try {
-      builder = builderFactory.newDocumentBuilder();
-    } catch (ParserConfigurationException e) {
-      Log.getErrorHandler().error("Error while preparing parser for xsd", e);
-    }
+  public ComponentSuite getComponentSuite() {
+    return componentSuite;
+  }
+
+  private boolean isLocalName(String localName) {
+    return localName.equals(parser.getLocalName());
+  }
+
+  private ComponentSuite createComponentSuite() {
+    String targetNS = parser.getAttributeValue(null, "targetNamespace");
+    String nsPrefix = parser.getNamespaceContext().getPrefix(targetNS);
+    return new ComponentSuite(targetNS, nsPrefix, null, null);
+  }
+
+  private ComponentClass createComponentClass() {
+    String name = parser.getAttributeValue(null, "name");
+    System.out.println("createComponentClass: " + name);
+    ComponentClass componentClass = new ComponentClass(null, name);
+    ccStack.push(componentClass);
+    return componentClass;
+  }
+
+  private void addXtypeToComponentClass() {
+    String xtype = parser.getAttributeValue(null, "name");
+    String typeName = afterColon(parser.getAttributeValue(null, "type"));
+    ComponentClass componentClass = ccStack.lastElement();
+    assert typeName.equals(componentClass.getFullClassName());
+    componentClass.setXtype(xtype);
+    System.out.printf("Added xtype %s to component class %s.%n", xtype, typeName);
+  }
+
+  private String afterColon(String typeName) {
+    return typeName.substring(typeName.indexOf(':')+1);
+  }
+
+  private void addSupertypeToComponentClass() {
+    String supertypeName = afterColon(parser.getAttributeValue(null, "base"));
+    ComponentClass componentClass = ccStack.lastElement();
+    componentClass.setSuperClassName(supertypeName);
+    System.out.printf("Added supertype %s to component class %s.%n", supertypeName, componentClass.getFullClassName());
+  }
+
+  private ConfigAttribute createConfigElementAttribute() {
+    String name = parser.getAttributeValue(null, "name");
+    ConfigAttribute attr = new ConfigAttribute(name, "Array");
+    ccStack.lastElement().addCfg(attr);
+    return attr;
+  }
+
+  private ConfigAttribute createConfigAttribute() {
+    String name = parser.getAttributeValue(null, "name");
+    String type = parser.getAttributeValue(null, "type");
+    ConfigAttribute attr = new ConfigAttribute(name, type);
+    ccStack.lastElement().addCfg(attr);
+    return attr;
   }
 
   public ComponentSuite scan(InputStream xsd) throws IOException {
+    XMLInputFactory factory = XMLInputFactory.newInstance();
+
     try {
-      Document document = null;
-      try {
-        if (builder != null) {
-          document = builder.parse(xsd);
+      parser = factory.createXMLStreamReader(xsd);
+      while (parser.hasNext()) { 
+        int event = parser.next();
+        if (XML_SCHEMA_URL.equals(parser.getNamespaceURI())) {
+          dispatch(event);
         }
-      } catch (SAXException e) {
+      }
+    } catch (XMLStreamException e) {
+      Log.getErrorHandler().error("Error while parsing XSD", e);
+    } finally {
+      try {
+        if (parser != null) {
+          parser.close();
+        }
+      } catch (XMLStreamException e) {
         Log.getErrorHandler().error("Error while parsing XSD", e);
       }
-      if (document != null) {
-        Element schemaElement = document.getDocumentElement();
-
-        ComponentSuite componentSuite = new ComponentSuite(
-          schemaElement.getAttribute("targetNamespace"),
-          schemaElement.lookupPrefix(schemaElement.getAttribute("targetNamespace")), null, null);
-        NodeList components = document.getElementsByTagNameNS(XML_SCHEMA_URL, "element");
-        for (int i = 0; i < components.getLength(); ++i) {
-          Element component = (Element) components.item(i);
-          String name = component.getAttribute("name");
-          String type = component.getAttribute("type");
-          int colonPos = type.indexOf(':');
-          if (colonPos != -1) {
-            type = type.substring(colonPos + 1);
-          }
-          if(!StringUtils.isEmpty(type)) {
-            //Log.getErrorHandler().info(String.format("Register class '%s' with xtype '%s'", type, name));
-            componentSuite.addComponentClass(new ComponentClass(name, type));
-          }
-        }
-        return componentSuite;
-      }
-      return null;
-    } finally {
-      xsd.close();
     }
+    return componentSuite;
+  }
+
+  private void dispatch(int event) {
+    switch (event) {
+      case XMLStreamConstants.START_ELEMENT:
+        if (isLocalName(SCHEMA)) {
+          componentSuite = createComponentSuite();
+          break;
+        } else if (!isInsideComplexType && isLocalName(COMPLEX_TYPE)) {
+          isInsideComplexType = true;
+          createComponentClass();
+          break;
+        } else if (isLocalName(ELEMENT)) {
+          if (isInsideComplexType) {
+            createConfigElementAttribute();
+            isInsideCfg = true;
+          } else {
+            addXtypeToComponentClass();
+          }
+        } else  if (isInsideComplexType && !isInsideCfg && isLocalName(EXTENSION)) {
+          addSupertypeToComponentClass();
+        } else if (isLocalName(ATTRIBUTE)) {
+          createConfigAttribute();
+        }
+        break;
+      case XMLStreamConstants.END_ELEMENT:
+        if (isLocalName(SCHEMA)) {
+          for (ComponentClass c : ccStack) {
+            componentSuite.addComponentClass(c);
+          }
+        } else if (isLocalName(COMPLEX_TYPE)) {
+          isInsideComplexType = false;
+        } else if (isLocalName(ELEMENT)) {
+          isInsideCfg = false;
+        }
+
+        break;
+      case XMLStreamConstants.CHARACTERS:
+
+        break;
+      case XMLStreamConstants.CDATA:
+
+        break;
+    } // end switch
   }
 
 }
