603dc4cd0f307b155de7fe72d1933db3e787e68e
Frank Wienberg <frank.wienberg@coremedia.com>
2010-04-22 12:50:15 +0100

    AS3 instead of JS inside EXML: full type checking and completion support. Known bug: if complection triggers import, the import is added as inline code :-(
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 174132]

--- jangaroo-idea-9/ext-xml-idea-plugin/src/main/java/net/jangaroo/ide/idea/exml/ExmlProjectComponent.java
+++ jangaroo-idea-9/ext-xml-idea-plugin/src/main/java/net/jangaroo/ide/idea/exml/ExmlProjectComponent.java
@@ -1,37 +1,44 @@
 /*
  * Copyright 2009 CoreMedia AG
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, 
+ * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an "AS
- * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
- * express or implied. See the License for the specific language 
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
 package net.jangaroo.ide.idea.exml;
 
-import com.intellij.lang.Language;
+import com.intellij.lang.ASTNode;
 import com.intellij.lang.javascript.JavaScriptSupportLoader;
+import com.intellij.lang.javascript.psi.ecmal4.JSClass;
 import com.intellij.openapi.compiler.CompilerManager;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.InjectedLanguagePlaces;
-import com.intellij.psi.LanguageInjector;
-import com.intellij.psi.PsiLanguageInjectionHost;
-import com.intellij.psi.PsiManager;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
 import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.XmlElementDescriptor;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Jangaroo Project Component.
@@ -44,57 +51,106 @@ public class ExmlProjectComponent implements ProjectComponent {
     this.project = project;
   }
 
+  private String getModuleRelativePath(VirtualFile file) {
+    ProjectFileIndex projectFileIndex = ProjectRootManager.getInstance(project).getFileIndex();
+    final Module module = projectFileIndex.getModuleForFile(file);
+    if (module != null) {
+      for (VirtualFile sourceRoot : ModuleRootManager.getInstance(module).getSourceRoots()) {
+        if (VfsUtil.isAncestor(sourceRoot, file, false)) {
+          return file.getPath().substring(sourceRoot.getPath().length() + 1);
+        }
+      }
+    }
+    return null;
+  }
+
   public void initComponent() {
     exmlc = new ExmlCompiler();
     // language injection: see http://www.jetbrains.net/devnet/message/5208687
     PsiManager.getInstance(project).registerLanguageInjector(new LanguageInjector() {
       public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost psiLanguageInjectionHost, @NotNull InjectedLanguagePlaces injectedLanguagePlaces) {
         //System.out.println("psiLanguageInjectionHost: "+psiLanguageInjectionHost);
-        if (psiLanguageInjectionHost.getContainingFile().getName().endsWith(".exml") && psiLanguageInjectionHost instanceof XmlAttributeValue) {
+        PsiFile psiFile = psiLanguageInjectionHost.getContainingFile();
+        if (psiFile.getName().endsWith(".exml") && psiLanguageInjectionHost instanceof XmlAttributeValue) {
+          VirtualFile exmlFile = psiFile.getOriginalFile().getVirtualFile();
+          if (exmlFile == null) {
+            return;
+          }
           XmlAttributeValue attributeValue = (XmlAttributeValue)psiLanguageInjectionHost;
           if (isImportClassAttribute(attributeValue)) {
             injectedLanguagePlaces.addPlace(JavaScriptSupportLoader.ECMA_SCRIPT_L4, new TextRange(1, attributeValue.getTextRange().getLength()-1), "import ", ";");
           } else {
             String text = attributeValue.getText();
             if (text.startsWith("\"{") && text.endsWith("}\"")) {
-              TextRange innerRange = new TextRange(2, attributeValue.getTextRange().getLength() - 2);
-              injectedLanguagePlaces.addPlace(getAS3(), innerRange, "function(config){return (", ");}");
+
+              ASTNode node = attributeValue.getParent().getNode();
+              while (node != null && node.getTreeParent() instanceof XmlTag && !"component".equals(((XmlTag)node.getTreeParent()).getLocalName())) {
+                node = node.getTreeParent();
+              }
+              if (node instanceof XmlTag) {
+                XmlTag xmlTag = (XmlTag)node;
+
+                // find relative path to source root to determine package name:
+                String packageName = getModuleRelativePath(exmlFile.getParent()).replace('/', '.');
+                String className = exmlFile.getNameWithoutExtension();
+
+                StringBuilder codePrefix = new StringBuilder("package ").append(packageName).append("{\n");
+
+                String superClassName = findSuperClass(xmlTag);
+
+                // find and append imports:
+                List<String> imports = findImports(xmlTag);
+                if (superClassName != null) {
+                  imports.add(superClassName);
+                }
+                for (String importName : imports) {
+                  codePrefix.append("import ").append(importName).append(";\n");
+                }
+
+                codePrefix.append("public class ").append(className);
+                if (superClassName != null) {
+                  codePrefix.append(" extends ").append(superClassName);
+                }
+                codePrefix.append("{\n");
+
+                codePrefix.append("public function ").append(className).append("(config:*){\n  super({x:(");
+                String codeSuffix = ")});\n}\n}\n}\n";
+
+                TextRange innerRange = new TextRange(2, attributeValue.getTextRange().getLength() - 2);
+                injectedLanguagePlaces.addPlace(JavaScriptSupportLoader.ECMA_SCRIPT_L4, innerRange, codePrefix.toString(), codeSuffix);
+                // to inject JavaScript, we'd use
+                //injectedLanguagePlaces.addPlace(JavaScriptSupportLoader.JAVASCRIPT.getLanguage(), innerRange, "", "");
+              }
             }
           }
         }
       }
     });
-//    import com.intellij.patterns.PlatformPatterns;
-//    import com.intellij.patterns.InitialPatternCondition;
-//    import com.intellij.patterns.XmlElementPattern;
-//    import com.intellij.psi.PsiElement;
-//    import com.intellij.psi.PsiReference;
-//    import com.intellij.psi.PsiReferenceProvider;
-//    import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
-//    import com.intellij.util.ProcessingContext;
-//    import org.jetbrains.annotations.Nullable;
-//    ReferenceProvidersRegistry.getInstance(project).registerReferenceProvider(
-//      new XmlElementPattern(new InitialPatternCondition<XmlTag>(XmlTag.class) {
-//        @Override
-//        public boolean accepts(@Nullable Object o, ProcessingContext context) {
-//          return super.accepts(o, context) && "http://extjs.com/ext3".equals(((XmlTag)o).getNamespace());
-//        }
-//      }), 
-//      new PsiReferenceProvider() {
-//        @NotNull
-//        @Override
-//        public PsiReference[] getReferencesByElement(@NotNull PsiElement psiElement, @NotNull ProcessingContext processingContext) {
-//          //System.out.println("getReferencesByElement("+psiElement.getText()+")");
-//          return new PsiReference[0];
-//        }
-//      }
-//     );
   }
 
-  private static Language getAS3() {
-    return JavaScriptSupportLoader.JAVASCRIPT.getLanguage();
-    //return JavaScriptSupportLoader.ECMA_SCRIPT_L4;
-    //return ((LanguageFileType)FileTypeManager.getInstance().getFileTypeByExtension("as")).getLanguage();
+  private static String findSuperClass(XmlTag xmlTag) {
+    XmlElementDescriptor descriptor = xmlTag.getDescriptor();
+    String superClassName = null;
+    if (descriptor != null && descriptor.getDeclaration() instanceof JSClass) {
+      JSClass jsClass = (JSClass)descriptor.getDeclaration();
+      superClassName = jsClass.getQualifiedName();
+    }
+    return superClassName;
+  }
+
+  private static List<String> findImports(XmlTag xmlTag) {
+    List<String> imports = new ArrayList<String>();
+    PsiElement sibling = xmlTag.getParent().getFirstChild();
+    while (sibling != null) {
+      if (sibling instanceof XmlTag) {
+        XmlTag topLevelXmlTag = (XmlTag)sibling;
+        if ("import".equals(topLevelXmlTag .getLocalName())) {
+          imports.add(topLevelXmlTag .getAttributeValue("class"));
+        }
+      }
+      sibling = sibling.getNextSibling();
+    }
+    return imports;
   }
 
   private static boolean isImportClassAttribute(XmlAttributeValue attributeValue) {
