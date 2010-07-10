27fbeda26e3d53aa81f026e88731d6d191a279af
Frank Wienberg <frank.wienberg@coremedia.com>
2010-07-10 10:16:10 +0100

    Class must be defined top-level or the dynamic class loader will try to load Class.js.
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 178652]

--- jangaroo-core/jangaroo-runtime/src/main/joo-js/scripts/classes/joo/Class.js
+++ jangaroo-core/jangaroo-runtime/src/main/joo-js/scripts/classes/joo/Class.js
@@ -1,5 +1,5 @@
 // simulate ActionScript's Class object for type casts and "is"
-joo.Class = function joo$Class(c){return c;};
-joo.Class.$class = {
+Class = function joo$Class(c){return c;};
+Class.$class = {
   isInstance: function(f){return typeof f=="function";}
 };
