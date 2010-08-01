d83e6e0797201d42167ad11ced0a0f768094a2c3
Frank Wienberg <frank.wienberg@coremedia.com>
2010-08-01 21:31:16 +0100

    Additional init calls generated after compiler fix lead to Class.init() being called, so I added an empty init() method to faked class declaration of class Class.
    [git-p4: depot-paths = "//coremedia/jangaroo/": change = 179484]

--- jangaroo-core/jangaroo-runtime/src/main/joo-js/scripts/classes/joo/Class.js
+++ jangaroo-core/jangaroo-runtime/src/main/joo-js/scripts/classes/joo/Class.js
@@ -1,5 +1,6 @@
 // simulate ActionScript's Class object for type casts and "is"
 Class = function joo$Class(c){return c;};
 Class.$class = {
+  init: function(){},
   isInstance: function(f){return typeof f=="function";}
 };
