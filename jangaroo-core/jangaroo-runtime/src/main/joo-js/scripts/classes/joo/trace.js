426968b212d8368d3ed8e34932ccaa6ea4ebdebf
Frank Wienberg <frank.wienberg@coremedia.com>
2010-10-01 10:56:08 +0200

    I wanted to take advantage of Firebug's differnt console log levels without changing AS3's trace() method signature. Pragmatic solution is that if you start your trace output with one of [INFO], [WARN], or [ERROR], the corresponding Firebug console method (info(), warn(), or error()) is invoked with the remainder of the message.

--- jangaroo-core/jangaroo-runtime/src/main/joo-js/scripts/classes/joo/trace.js
+++ jangaroo-core/jangaroo-runtime/src/main/joo-js/scripts/classes/joo/trace.js
@@ -1,9 +1,17 @@
 // function trace(msg : String) : void
 joo.trace = function joo$trace(msg) {
+  var logLevelMatches = msg.match(/^\[([A-Z]+)\]\s*(.*)$/);
+  var logLevel = logLevelMatches ? logLevelMatches[1].toLowerCase() : null;
   msg = "AS3: " + msg;
   var console;
   if ((console = joo.getQualifiedObject("console")) && console.log) {
-    console.log(msg);
+    // Firebug supports different log levels:
+    if (!console[logLevel]) {
+      logLevel = 'log';
+    } else {
+      msg = "AS3: " + logLevelMatches[2];
+    }
+    console[logLevel](msg);
   } else if ((console = joo.getQualifiedObject("runtime")) && console.trace) {
     console.trace(msg);
   } else if (console = joo.getQualifiedObject("trace")) {
