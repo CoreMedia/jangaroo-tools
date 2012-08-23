23b874fa0d9a3de1e468c6e6319325abf9f77a10
Frank Wienberg <frank.wienberg@coremedia.com>
2012-08-23 11:24:11 +0200

    fixes JOO-38 http://jangaroo.myjetbrains.com/youtrack/issue/JOO-38 On the fly, making int and uint coercion and "is" check much more efficient (thanks stackoverflow!).

--- jangaroo/jangaroo-runtime/src/main/joo-js/joo/classes/uint.js
+++ jangaroo/jangaroo-runtime/src/main/joo-js/joo/classes/uint.js
@@ -1,16 +1,7 @@
 (function(theGlobalObject) {
-  var MAX_VALUE =  4294967295;
-  theGlobalObject['uint'] = theGlobalObject['$$uint'] = function $uint(num) {
-    num = Number(num);
-    if (num > 0) {
-      return Math.floor(num);
-    }
-    if (num <= -1) {
-      return MAX_VALUE + Math.ceil(num) + 1;
-    }
-    // also handles NaN
-    return 0;
+  theGlobalObject['uint'] = theGlobalObject['$$uint'] = function $$uint(num) {
+    return Number(num) >>> 0;
   };
-  theGlobalObject['uint'].MAX_VALUE =  MAX_VALUE;
-  theGlobalObject['uint'].MIN_VALUE =  0;
+  theGlobalObject['uint'].MAX_VALUE = Math.pow(2, 32);
+  theGlobalObject['uint'].MIN_VALUE = 0;
 })(this);
