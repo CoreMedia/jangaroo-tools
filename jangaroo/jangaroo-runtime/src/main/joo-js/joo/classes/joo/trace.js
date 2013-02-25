d4256e20b7c35495686098a8d493e290753d961f
Frank Wienberg <frank.wienberg@coremedia.com>
2013-02-25 17:03:23 +0100

    fixes JOO-58 trace(undefined) and trace(null) print empty string http://jangaroo.myjetbrains.com/youtrack/issue/JOO-58
    On the fly, factored out console object detection to happen
    only once, and made everything more robust (hopefully).
    What we really should do is separate a console polyfill from
    the AS3 trace() implementation.

--- jangaroo/jangaroo-runtime/src/main/joo-js/joo/classes/joo/trace.js
+++ jangaroo/jangaroo-runtime/src/main/joo-js/joo/classes/joo/trace.js
@@ -1,25 +1,46 @@
 // function trace(...msg) : void
 if (typeof joo.trace !== "function") {
-  joo.trace = function joo$trace() {
-    var msg = Array.prototype.join.call(arguments, " ");
-    var logLevelMatches = msg.match(/^\[(LOG|INFO|WARN|ERROR)\]\s*(.*)$/);
-    var logLevel = logLevelMatches ? logLevelMatches[1].toLowerCase() : null;
-    msg = "AS3: " + msg;
-    var console;
-    if ((console = joo.getQualifiedObject("console")) && console.log) {
-      // Firebug supports different log levels:
-      if (!console[logLevel]) {
-        logLevel = 'log';
-      } else {
-        msg = "AS3: " + logLevelMatches[2];
+  (function(theGlobalObject) {
+    var console, defaultLogLevel;
+    function lookup(consoleObject, traceFunctionName) {
+      if (consoleObject && consoleObject[traceFunctionName]) {
+        console = consoleObject;
+        defaultLogLevel = traceFunctionName;
+        return true;
       }
-      console[logLevel](msg);
-    } else if ((console = joo.getQualifiedObject("runtime")) && console.trace) {
-      console.trace(msg);
-    } else if (console = joo.getQualifiedObject("trace")) {
-      console(msg);
-    } else if (console = joo.getQualifiedObject("opera")) {
-      console.postError(msg);
+      return false;
     }
-  };
+    try {
+      lookup(theGlobalObject.console, "log") ||
+              lookup(theGlobalObject.runtime, "trace") ||
+              lookup(theGlobalObject, "trace") ||
+      lookup(theGlobalObject.opera, "postError");
+    } catch (e) {
+      // ignore
+    }
+    var LOG_LEVEL_PATTERN = /^\[(LOG|DEBUG|TRACE|INFO|WARN|ERROR)\]\s*(.*)$/;
+    joo.trace = !console ? function() {} : function joo$trace() {
+      // don't use Array.prototype.map, as it is not available in all browsers and has not yet been polyfilled:
+      var params = [];
+      for (var i = 0; i < arguments.length; i++) {
+        params.push(String(arguments[i]));
+      }
+      var msg = params.join(" ");
+      var logLevel = defaultLogLevel;
+      var logLevelMatches = msg.match(LOG_LEVEL_PATTERN);
+      if (logLevelMatches) {
+        var specialLogLevel = logLevelMatches[1].toLowerCase();
+        try {
+          if (console[specialLogLevel]) {
+            // special log level supported by this console:
+            logLevel = specialLogLevel;
+            msg = logLevelMatches[2];
+          }
+        } catch (e) {
+          // ignore
+        }
+      }
+      console[logLevel]("AS3: " + msg);
+    };
+  })(this);
 }
