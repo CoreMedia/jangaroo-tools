7b4c4f3d0b69bb08644d7e8f9bafac383e22df0f
Frank Wienberg <frank.wienberg@coremedia.com>
2013-03-27 12:55:46 +0100

    expected result of new test for JOO-64 has to be converted into Joo-3 format to make test green

--- jangaroo/jangaroo-compiler/src/test/resources/expected/package1/StaticAndNonStatic.js
+++ jangaroo/jangaroo-compiler/src/test/resources/expected/package1/StaticAndNonStatic.js
@@ -1,12 +1,28 @@
-joo.classLoader.prepare("package package1",/* {*/
+define(["exports","as3-rt/AS3"], function($exports,AS3) { AS3.compilationUnit($exports, function($primaryDeclaration){/*package package1 {
 
 /**
  * Retest for JOO-64.
- */
-"public class StaticAndNonStatic",1,function($$private){var $1=package1;return[ 
+ * /
+public class StaticAndNonStatic {
 
-  "public var",{ StaticAndNonStatic/*:String*/:null},function(){
+  public var StaticAndNonStatic:String;*/function static$0(){
   
-  new $1.StaticAndNonStatic();},
-undefined];},[],[], "@runtimeVersion", "@version"
-);
+  new StaticAndNonStatic();}/*
+}*/function StaticAndNonStatic() {}/*
+}
+
+============================================== Jangaroo part ==============================================*/
+    $primaryDeclaration(AS3.class_({
+      package_: "package1",
+      class_: "StaticAndNonStatic",
+      members: {
+        StaticAndNonStatic: {
+          value: null,
+          writable: true
+        },
+        constructor: StaticAndNonStatic
+      }
+    }));
+    static$0();
+  });
+});
