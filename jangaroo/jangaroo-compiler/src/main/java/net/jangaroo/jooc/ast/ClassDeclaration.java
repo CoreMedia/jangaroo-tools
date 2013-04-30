CoreMedia/jangaroo-tools

commit 2023fee385ba2f185efa0ab030a132c6a2492582

ClassDeclaration: Don't resolve static members, as they might hide non-static ones and lead to an unjustified compiler error. Ide: In the same sense, don't throw errors when finding a static member when asked for a non-static one, as there might be a non-static member in the superclass.

jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/ast/ClassDeclaration.java
@@ -264,9 +264,6 @@ public class ClassDeclaration extends IdeDeclaration {
     final int chainSize = chain.size();
     chain.add(classDecl);
     IdeDeclaration declaration = classDecl.getMemberDeclaration(ide);
-    if (declaration == null) {
-      declaration = classDecl.getStaticMemberDeclaration(ide);
-    }
     if (declaration == null && classDecl.getSuperType() != null) {
       declaration = resolvePropertyInSuper(ide, classDecl, visited, chain, classDecl.getSuperType().getIde());
     }

jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/ast/Ide.java
@@ -15,7 +15,6 @@
 
 package net.jangaroo.jooc.ast;
 
-import net.jangaroo.jooc.CompilerError;
 import net.jangaroo.jooc.JooSymbol;
 import net.jangaroo.jooc.Jooc;
 import net.jangaroo.jooc.Scope;
@@ -181,15 +180,19 @@ public class Ide extends NodeImplBase {
   public IdeDeclaration getDeclaration(boolean errorIfUndeclared) {
     if (declaration == null) {
       declaration = getScope().lookupDeclaration(this);
+      if (declaration != null) {
+        if (declaration.getClassDeclaration() != getScope().getClassDeclaration()) {
+          if (declaration.isPrivate()) {
+            // private member access
+            declaration = null;
+          } else if (declaration.isProtected() && !getScope().getClassDeclaration().isSubclassOf(declaration.getClassDeclaration())) {
+            // protected member access of non-superclass
+            declaration = null;
+          }
+        }
+      }
       if (declaration == null) {
         declaration = NULL_DECL; // prevent multiple lookups when called with !errorIfUndeclared multiple times
-      } else if (declaration.getClassDeclaration() != getScope().getClassDeclaration()) {
-        if (declaration.isPrivate()) {
-          throw new CompilerError(this.getSymbol(), "private member access");
-        }
-        if (declaration.isProtected() && !getScope().getClassDeclaration().isSubclassOf(declaration.getClassDeclaration())) {
-          throw new CompilerError(this.getSymbol(), "protected member access of non-superclass");
-        }
       }
     }
     final IdeDeclaration result = declaration == NULL_DECL ? null : declaration; // NOSONAR no equals here

commit b0348b204883ef264db65769f503452980e85890

fix statics test, which broke with commit 2023fee385ba2f185efa0ab030a132c6a2492582
Solution is still not optimal, since we should really distinguish between static and non-static
members through dedicated types for both. In that sense, a class defines two types: one for
its non-static members, including inheritance, and one for its static members, where static
members of superclasses are only in scope of the subclass, but not inherited. Currently, we
cannot express the latter type, so we have to live with this workaround.

jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/ast/ClassDeclaration.java
@@ -250,10 +250,14 @@ public class ClassDeclaration extends IdeDeclaration {
    * @return a non-static member if found, null otherwise
    */
   public IdeDeclaration resolvePropertyDeclaration(String ide) {
-    return resolvePropertyDeclaration1(ide, this, new HashSet<ClassDeclaration>(), new LinkedList<ClassDeclaration>());
+    IdeDeclaration ideDeclaration = resolvePropertyDeclaration1(ide, this, new HashSet<ClassDeclaration>(), new LinkedList<ClassDeclaration>(), false);
+    if (ideDeclaration == null) {
+      ideDeclaration = resolvePropertyDeclaration1(ide, this, new HashSet<ClassDeclaration>(), new LinkedList<ClassDeclaration>(), true);
+    }
+    return ideDeclaration;
   }
 
-  private IdeDeclaration resolvePropertyDeclaration1(String ide, ClassDeclaration classDecl, Set<ClassDeclaration> visited, Deque<ClassDeclaration> chain) {
+  private IdeDeclaration resolvePropertyDeclaration1(String ide, ClassDeclaration classDecl, Set<ClassDeclaration> visited, Deque<ClassDeclaration> chain, boolean inStatic) {
     if (visited.contains(classDecl)) {
       if (chain.contains(classDecl)) {
         throw new CompilerError(classDecl.getSymbol(), "cyclic superclass chain");
@@ -263,14 +267,14 @@ public class ClassDeclaration extends IdeDeclaration {
     visited.add(classDecl);
     final int chainSize = chain.size();
     chain.add(classDecl);
-    IdeDeclaration declaration = classDecl.getMemberDeclaration(ide);
+    IdeDeclaration declaration = inStatic ? classDecl.getStaticMemberDeclaration(ide) : classDecl.getMemberDeclaration(ide);
     if (declaration == null && classDecl.getSuperType() != null) {
-      declaration = resolvePropertyInSuper(ide, classDecl, visited, chain, classDecl.getSuperType().getIde());
+      declaration = resolvePropertyInSuper(ide, classDecl, visited, chain, classDecl.getSuperType().getIde(), inStatic);
     }
     if (declaration == null && classDecl.getOptImplements() != null) {
       CommaSeparatedList<Ide> implemented = classDecl.getOptImplements().getSuperTypes();
       while (implemented != null && declaration == null) {
-        declaration = resolvePropertyInSuper(ide, classDecl, visited, chain, implemented.getHead());
+        declaration = resolvePropertyInSuper(ide, classDecl, visited, chain, implemented.getHead(), inStatic);
         implemented = implemented.getTail();
       }
     }
@@ -283,13 +287,13 @@ public class ClassDeclaration extends IdeDeclaration {
                                                 final ClassDeclaration classDecl,
                                                 final Set<ClassDeclaration> visited,
                                                 final Deque<ClassDeclaration> chain,
-                                                final Ide superIde) {
+                                                final Ide superIde, boolean inStatic) {
     IdeDeclaration superClassDecl = superIde.getDeclaration(false);
     if (superClassDecl != null) {
       if (!(superClassDecl instanceof ClassDeclaration)) {
         throw new CompilerError(classDecl.getOptExtends().getSuperClass().getSymbol(), "expected class identifier");
       }
-      return resolvePropertyDeclaration1(ide, (ClassDeclaration) superClassDecl, visited, chain);
+      return resolvePropertyDeclaration1(ide, (ClassDeclaration) superClassDecl, visited, chain, inStatic);
     }
     return null;
   }