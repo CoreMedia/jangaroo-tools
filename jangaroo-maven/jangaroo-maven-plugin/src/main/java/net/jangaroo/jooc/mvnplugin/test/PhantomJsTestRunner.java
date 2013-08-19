af34b1171cc011f85022efae58a3918222668386
Moritz Kleine <moritz.kleine@coremedia.com>
2013-08-19 10:09:32 +0200

    don't try to execute phantomjs to determine if it exists - builds sometimes fail because "phantomjs.exe -v" hang - maybe related to http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4770092

--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/test/PhantomJsTestRunner.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/test/PhantomJsTestRunner.java
@@ -1,5 +1,7 @@
 package net.jangaroo.jooc.mvnplugin.test;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.SystemUtils;
 import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.util.cli.CommandLineException;
 import org.codehaus.plexus.util.cli.CommandLineUtils;
@@ -7,13 +9,19 @@ import org.codehaus.plexus.util.cli.Commandline;
 import org.codehaus.plexus.util.cli.StreamConsumer;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.codehaus.plexus.util.StringUtils.isBlank;
+
 public class PhantomJsTestRunner {
 
   public static final Pattern LOG_LEVEL_PATTERN = Pattern.compile("^\\[([A-Z]+)\\]\\s*(.*)$");
+
+  private static volatile Boolean phantomjsExecutableFound;
+
   private final String testPageUrl;
   private String testResultFilename;
   private final String phantomjs;
@@ -30,12 +38,21 @@ public class PhantomJsTestRunner {
    * @param log                 the maven log
    */
   public PhantomJsTestRunner(String phantomjs, String testPageUrl, String testResultFilename, String testRunner, int timeout, Log log) {
-    this.phantomjs = phantomjs;
+    this.phantomjs = makeOsSpecific(phantomjs);
     this.testPageUrl = testPageUrl;
     this.testResultFilename = testResultFilename;
     this.testRunner = testRunner;
     this.timeout = timeout;
     this.log = log;
+  }
+
+  private static String makeOsSpecific(String phantomjs) {
+    if(SystemUtils.IS_OS_WINDOWS) {
+      if(!isBlank(phantomjs) && phantomjs.charAt(phantomjs.length() - 4) != '.') {
+        return phantomjs +  ".exe";
+      }
+    }
+    return phantomjs;
   }
 
   public boolean execute() throws CommandLineException {
@@ -88,32 +105,34 @@ public class PhantomJsTestRunner {
             testResultFilename != null &&
                     testRunner != null &&
                     phantomjs != null &&
-                    (new File(phantomjs).canExecute() || canExecutePhantomJs());
+                    isExecutableAvailable();
+  }
+
+  private boolean isExecutableAvailable() {
+    if(phantomjsExecutableFound == null) {
+      synchronized (PhantomJsTestRunner.class) {
+        phantomjsExecutableFound = (new File(phantomjs).canExecute() || canExecutePhantomJs());
+      }
+    }
+    return phantomjsExecutableFound;
   }
 
   private boolean canExecutePhantomJs() {
-    final Commandline commandline = createCommandLine();
-    commandline.addArguments(new String[]{"--version"});
-    final StringBuilder buffer = new StringBuilder();
-    final StreamConsumer consumer = new StreamConsumer() {
-      @Override
-      public void consumeLine(String line) {
-        buffer.append(line);
-
+    final String path = System.getenv("PATH");
+    final String[] directoryNames = StringUtils.split(path, File.pathSeparatorChar);
+    for (String directoryName : directoryNames) {
+      final File[] files = new File(directoryName).listFiles(new FilenameFilter() {
+        @Override
+        public boolean accept(File dir, String name) {
+          return phantomjs.equals(name);
+        }
+      });
+      if(!(files == null || files.length == 0)) {
+        return true;
       }
-    };
-    boolean canExecute = false;
-    try {
-      canExecute = CommandLineUtils.executeCommandLine(commandline, consumer, consumer, 1) == 0;
-      if (canExecute) {
-        log.info("Found phantomjs version: " + buffer.toString());
-      } else {
-        log.info("Cannot determine phantomjs version: " + buffer.toString());
-      }
-    } catch (CommandLineException e) {
-      log.error("cannot execute phantomjs", e);
     }
-    return canExecute;
+    log.warn(phantomjs + " not found in " + path);
+    return false;
   }
 
   private Commandline createCommandLine() {
