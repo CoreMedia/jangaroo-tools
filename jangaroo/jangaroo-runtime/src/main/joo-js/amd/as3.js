5c670b11c2c8dfea2eff6ac743fe2f54baf241c9
Frank Wienberg <frank.wienberg@coremedia.com>
2015-03-03 16:54:30 +0100

    Fix: remove remains of old debugging approach from as3! RequireJS plugin

--- jangaroo/jangaroo-runtime/src/main/joo-js/amd/as3.js
+++ jangaroo/jangaroo-runtime/src/main/joo-js/amd/as3.js
@@ -1,11 +1,4 @@
 define(function() {
-  var debug = "false";
-  if (typeof location === "object" && typeof location.hash === "string") {
-    var match = location.hash.match(/(?:^#|&)joo.debug(?:=(true|false|linked)|&|$)/);
-    if (match) {
-      debug = match[1] || "true";
-    }
-  }
   "use strict";
   return {
     normalize: function (name, normalize) {
@@ -13,20 +6,9 @@ define(function() {
     },
 
     load: function (name, req, load, config) {
-      function loadAndInit() {
-        req([name], function (value) {
-          load(value._);
-        });
-      }
-      if (config.isBuild) {
-        loadAndInit();
-      } else {
-        switch (debug) {
-          case "true":   loadAndInit(); break;
-          case "linked": req(["application"], loadAndInit); break;
-          case "false":  req(["application-min"], loadAndInit); break;
-        }
-      }
+      req([name], function (value) {
+        load(value._);
+      });
     }
   };
 });
