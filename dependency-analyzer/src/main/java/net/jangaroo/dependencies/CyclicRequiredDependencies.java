1e1bddd8c52a1753a7683d3d7a2dc961588b504f
Olaf Kummer <olaf.kummer@coremedia.com>
2016-01-05 09:41:18 +0100

    towards an analysis tool for cyclic dependencies
    Works by traversing a directory tree, scanning for compiled jangaroo classes
    and building a graphml file for yEd showing all classes in cycles.
    Initial version is based on the file format for new class loading,
    but will be converted to the classic format in the following commits.

---
+++ dependency-analyzer/src/main/java/net/jangaroo/dependencies/CyclicRequiredDependencies.java
@@ -0,0 +1,136 @@
+package net.jangaroo.dependencies;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multiset;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CyclicRequiredDependencies {
+
+  public static final String PATH_PREFIX = "/target/classes/META-INF/resources/joo/classes/";
+  public static final String PATH_SUFFIX = ".js";
+  public static final String JANGAROO_PART_MARKER = "============================================== Jangaroo part ==============================================*/";
+  public static final String REQUIRES_MARKER = "      requires: [";
+  public static final String REQUIRES_PREFIX = "        \"AS3.";
+
+  public static void main(String[] args) throws IOException {
+    String baseDir = args[0];
+    List<File> files = new ArrayList<>();
+    fillInCompiledClassFiles(new File(baseDir), files);
+
+    Multimap<String,String> requires = HashMultimap.create();
+    for (File file : files) {
+      fillInRequires(file, requires);
+    }
+
+    Set<String> classesInCycles = getNodesInCycles(requires);
+    requires.keySet().retainAll(classesInCycles);
+    requires.values().retainAll(classesInCycles);
+
+    System.out.println("All classes: " + requires);
+    System.out.println("Classes in cycles: " + classesInCycles);
+
+    File outFile = new File(args[1]);
+    try (PrintWriter writer = new PrintWriter(new FileWriter(outFile))) {
+      writeGraph(writer, requires);
+    }
+  }
+
+  private static void fillInCompiledClassFiles(File baseDir, List<File> files) {
+    for (File file : baseDir.listFiles()) {
+      if (file.isDirectory()) {
+        fillInCompiledClassFiles(file, files);
+      } else {
+        String path = file.getPath();
+        if (path.contains(PATH_PREFIX) && path.endsWith(PATH_SUFFIX)) {
+          files.add(file);
+        }
+      }
+    }
+  }
+
+  private static void fillInRequires(File file, Multimap<String, String> requires) throws IOException {
+    String path = file.getPath();
+    String className = path
+            .substring(path.indexOf(PATH_PREFIX) + PATH_PREFIX.length(), path.length() - PATH_SUFFIX.length())
+            .replace('/', '.');
+    List<String> lines = Files.readLines(file, Charsets.UTF_8);
+    int jangarooPartStart = lines.indexOf(JANGAROO_PART_MARKER);
+    if (jangarooPartStart == -1) {
+      return;
+    }
+    int requiresStart = lines.subList(jangarooPartStart, lines.size()).indexOf(REQUIRES_MARKER);
+    if (requiresStart == -1) {
+      return;
+    }
+    int pos = jangarooPartStart + requiresStart + 1;
+    while (pos < lines.size()) {
+      String line = lines.get(pos);
+      if (!line.startsWith(REQUIRES_PREFIX)) {
+        break;
+      }
+      int quotePos = line.indexOf('"', REQUIRES_PREFIX.length());
+      String requiredClassName = line.substring(REQUIRES_PREFIX.length(), quotePos);
+      requires.put(className, requiredClassName);
+      pos++;
+    }
+  }
+
+  private static Set<String> getNodesInCycles(Multimap<String, String> edges) {
+    Multiset<String> classNames = edges.keys();
+    Set<String> result = new HashSet<>();
+    for (String className : classNames) {
+      if (getAllSuccessors(className, edges).contains(className)) {
+        result.add(className);
+      }
+    }
+    return result;
+  }
+
+  private static Set<String> getAllSuccessors(String nodeId, Multimap<String, String> edges) {
+    Set<String> result = new HashSet<>();
+    fillInSuccessors(nodeId, edges, result);
+    return result;
+  }
+
+  private static void fillInSuccessors(String nodeId, Multimap<String, String> edges, Set<String> result) {
+    Collection<String> successors = edges.get(nodeId);
+    for (String successor : successors) {
+      if (result.add(successor)) {
+        fillInSuccessors(successor, edges, result);
+      }
+    }
+  }
+
+  private static void writeGraph(PrintWriter writer, Multimap<String, String> requires) {
+    writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    writer.println("<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:java=\"http://www.yworks.com/xml/yfiles-common/1.0/java\" xmlns:sys=\"http://www.yworks.com/xml/yfiles-common/markup/primitives/2.0\" xmlns:x=\"http://www.yworks.com/xml/yfiles-common/markup/2.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:y=\"http://www.yworks.com/xml/graphml\" xmlns:yed=\"http://www.yworks.com/xml/yed/3\" xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd\">");
+    writer.println("<key for=\"node\" id=\"d6\" yfiles.type=\"nodegraphics\"/>");
+    for (String nodeId : requires.keys()) {
+      writer.println("    <node id=\"" + nodeId + "\">");
+      writer.println("      <data key=\"d6\">");
+      writer.println("        <y:ShapeNode>");
+      writer.println("          <y:Geometry height=\"20.0\" width=\"" + 8 * nodeId.length() + "\" x=\"765.099609375\" y=\"-442.9166666666667\"/>\n");
+      writer.println("          <y:NodeLabel>" + nodeId + "</y:NodeLabel>");
+      writer.println("        </y:ShapeNode>");
+      writer.println("      </data>");
+      writer.println("    </node>");
+    }
+    for (Map.Entry<String, String> entry : requires.entries()) {
+      writer.println("    <edge source=\"" + entry.getKey() + "\" target=\"" + entry.getValue() + "\"/>");
+    }
+    writer.println("</graphml>");
+  }
+}
