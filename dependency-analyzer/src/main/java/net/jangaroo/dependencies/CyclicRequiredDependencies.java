999ed375a15c38954534809525fa9b93d0e59c00
Olaf Kummer <olaf.kummer@coremedia.com>
2016-01-07 15:49:59 +0100

    restrict cycle detection to those cycles that contain classes with static initializers

--- dependency-analyzer/src/main/java/net/jangaroo/dependencies/CyclicRequiredDependencies.java
+++ dependency-analyzer/src/main/java/net/jangaroo/dependencies/CyclicRequiredDependencies.java
@@ -11,8 +11,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Deque;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -31,17 +32,16 @@ public class CyclicRequiredDependencies {
       fillInCompiledClassFiles(new File(baseDir), files);
 
       Multimap<String, String> requires = HashMultimap.create();
+      Set<String> staticallyInitialized = new HashSet<>();
       for (File file : files) {
-        fillInRequires(file, requires);
+        analyzeFile(file, requires, staticallyInitialized);
       }
 
-      Set<String> classesInCycles = getNodesInCycles(requires);
-      requires.keySet().retainAll(classesInCycles);
-      requires.values().retainAll(classesInCycles);
+      Set<String> classesInCycles = getNodesInMarkedCycles(requires, staticallyInitialized);
 
       File outFile = new File(args[1]);
       try (PrintWriter writer = new PrintWriter(new FileWriter(outFile))) {
-        writeGraph(writer, requires);
+        writeGraph(writer, requires, classesInCycles, staticallyInitialized);
       }
     }
   }
@@ -59,7 +59,7 @@ public class CyclicRequiredDependencies {
     }
   }
 
-  private static void fillInRequires(File file, Multimap<String, String> requires) throws IOException {
+  private static void analyzeFile(File file, Multimap<String, String> requires, Set<String> staticallyInitialized) throws IOException {
     String path = file.getPath();
     String className = path
             .substring(path.indexOf(PATH_PREFIX) + PATH_PREFIX.length(), path.length() - PATH_SUFFIX.length())
@@ -68,8 +68,12 @@ public class CyclicRequiredDependencies {
 
     int lastBrace = -1;
     for (int i = 0; i < lines.size(); i++) {
-      if (lines.get(i).indexOf('}') != -1) {
+      String line = lines.get(i);
+      if (line.indexOf('}') != -1) {
         lastBrace = i;
+      }
+      if (line.contains("HAS_STATIC_CODE")) {
+        staticallyInitialized.add(className);
       }
     }
     if (lastBrace == -1) {
@@ -82,46 +86,63 @@ public class CyclicRequiredDependencies {
     String dependenciesString = descriptorLine.substring(startPos + 1, endPos);
     String[] dependencies = dependenciesString.split("[\",]+");
     for (String dependency : dependencies) {
-      if (!dependency.isEmpty()) {
+      if (!dependency.isEmpty() && !dependency.equals(className)) {
         requires.put(className, dependency);
       }
     }
   }
 
-  private static Set<String> getNodesInCycles(Multimap<String, String> edges) {
+  private static Set<String> getNodesInMarkedCycles(Multimap<String, String> edges, Set<String> markedNodes) {
     Multiset<String> classNames = edges.keys();
     Set<String> result = new HashSet<>();
     for (String className : classNames) {
-      if (getAllSuccessors(className, edges).contains(className)) {
+      if (isContainedInMarkedCycle(className, edges, markedNodes)) {
         result.add(className);
       }
     }
     return result;
   }
 
-  private static Set<String> getAllSuccessors(String nodeId, Multimap<String, String> edges) {
-    Set<String> result = new HashSet<>();
-    fillInSuccessors(nodeId, edges, result);
-    return result;
-  }
-
-  private static void fillInSuccessors(String nodeId, Multimap<String, String> edges, Set<String> result) {
-    Collection<String> successors = edges.get(nodeId);
-    for (String successor : successors) {
-      if (result.add(successor)) {
-        fillInSuccessors(successor, edges, result);
+  private static boolean isContainedInMarkedCycle(String node, Multimap<String, String> edges, Set<String> markedNodes) {
+    // Compute all reachable marked nodes.
+    Set<String> reachable = new HashSet<>();
+    Deque<String> todo = new LinkedList<>();
+    todo.addAll(edges.get(node));
+    while (!todo.isEmpty()) {
+      String current = todo.removeLast();
+      if (reachable.add(current)) {
+        todo.addAll(edges.get(current));
       }
     }
+    reachable.retainAll(markedNodes);
+
+    // Compute all reachable marked nodes and nodes reachable from those nodes.
+    todo.clear();
+    todo.addAll(reachable);
+    reachable.clear();
+    while (!todo.isEmpty()) {
+      String current = todo.removeLast();
+      if (reachable.add(current)) {
+        todo.addAll(edges.get(current));
+      }
+    }
+
+    return reachable.contains(node);
   }
 
-  private static void writeGraph(PrintWriter writer, Multimap<String, String> requires) {
+  private static void writeGraph(PrintWriter writer, Multimap<String, String> requires, Set<String> classesInCycles, Set<String> staticallyInitialized) {
     writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
     writer.println("<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:java=\"http://www.yworks.com/xml/yfiles-common/1.0/java\" xmlns:sys=\"http://www.yworks.com/xml/yfiles-common/markup/primitives/2.0\" xmlns:x=\"http://www.yworks.com/xml/yfiles-common/markup/2.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:y=\"http://www.yworks.com/xml/graphml\" xmlns:yed=\"http://www.yworks.com/xml/yed/3\" xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd\">");
     writer.println("<key for=\"node\" id=\"d6\" yfiles.type=\"nodegraphics\"/>");
-    for (String nodeId : requires.keys()) {
+    for (String nodeId : classesInCycles) {
       writer.println("    <node id=\"" + nodeId + "\">");
       writer.println("      <data key=\"d6\">");
       writer.println("        <y:ShapeNode>");
+      if (staticallyInitialized.contains(nodeId)) {
+        writer.println("        <y:Fill color=\"#FFA0A0\" transparent=\"false\"/>");
+      } else {
+        writer.println("        <y:Fill color=\"#A0FFA0\" transparent=\"false\"/>");
+      }
       writer.println("          <y:Geometry height=\"20.0\" width=\"" + 8 * nodeId.length() + "\" x=\"765.099609375\" y=\"-442.9166666666667\"/>\n");
       writer.println("          <y:NodeLabel>" + nodeId + "</y:NodeLabel>");
       writer.println("        </y:ShapeNode>");
@@ -129,7 +150,9 @@ public class CyclicRequiredDependencies {
       writer.println("    </node>");
     }
     for (Map.Entry<String, String> entry : requires.entries()) {
-      writer.println("    <edge source=\"" + entry.getKey() + "\" target=\"" + entry.getValue() + "\"/>");
+      if (classesInCycles.contains(entry.getKey()) && classesInCycles.contains(entry.getValue())) {
+        writer.println("    <edge source=\"" + entry.getKey() + "\" target=\"" + entry.getValue() + "\"/>");
+      }
     }
     writer.println("</graphml>");
   }
