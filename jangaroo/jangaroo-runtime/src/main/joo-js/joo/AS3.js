ecd6be6728b27de2a93f46e03c2af6abb6ad1ab5
Mark Michaelis <mark.michaelis@coremedia.com>
2016-01-14 13:23:44 +0100

    Revert "Fix mapping from JooOverrides namespace to JangarooOverrides"
    This reverts commit 7e72570d737f1a2385b6c76ad56863225a0e9f9f.

--- jangaroo/jangaroo-runtime/src/main/joo-js/joo/AS3.js
+++ jangaroo/jangaroo-runtime/src/main/joo-js/joo/AS3.js
@@ -109,7 +109,7 @@ Ext.ns("joo.localization");
 
 Ext.Loader.setPath({
   'AS3': 'joo/classes',
-  'JangarooOverrides': 'joo/overrides'
+  'JooOverrides': 'joo/overrides'
 });
 
 Ext.Class.registerPreprocessor('accessors', function (Class, data) {
