c6711b227d25a482f739be5cc2b1e133cd2b3917
Alexander Holtkamp <alexander.holtkamp@coremedia.com>
2016-02-19 17:50:36 +0100

    removed dedicated package.json templates for code and theme packages
    -could be merged into one
    -type will be set by the configurer
    -on the fly also enables sass for code packages

--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/sencha/configurer/DefaultSenchaCodePackageConfigurer.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/sencha/configurer/DefaultSenchaCodePackageConfigurer.java
@@ -1,14 +1,26 @@
 package net.jangaroo.jooc.mvnplugin.sencha.configurer;
 
+import org.apache.maven.plugin.MojoExecutionException;
+
 import java.io.InputStream;
+import java.util.Map;
 
 public class DefaultSenchaCodePackageConfigurer extends AbstractJsonInputStreamConfigurer {
 
   private static DefaultSenchaCodePackageConfigurer instance;
 
+  private static final String TYPE = "type";
+
+  @Override
+  public void configure(Map<String, Object> config) throws MojoExecutionException {
+    super.configure(config);
+
+    config.put(TYPE, "code");
+  }
+
   @Override
   protected InputStream getInputStream() {
-    return getClass().getResourceAsStream("default.code.package.json");
+    return getClass().getResourceAsStream("default.package.json");
   }
 
   public static DefaultSenchaCodePackageConfigurer getInstance() {
--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/sencha/configurer/DefaultSenchaThemePackageConfigurer.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/sencha/configurer/DefaultSenchaThemePackageConfigurer.java
@@ -1,14 +1,26 @@
 package net.jangaroo.jooc.mvnplugin.sencha.configurer;
 
+import org.apache.maven.plugin.MojoExecutionException;
+
 import java.io.InputStream;
+import java.util.Map;
 
 public class DefaultSenchaThemePackageConfigurer extends AbstractJsonInputStreamConfigurer {
 
   private static DefaultSenchaThemePackageConfigurer instance;
 
+  private static final String TYPE = "type";
+
+  @Override
+  public void configure(Map<String, Object> config) throws MojoExecutionException {
+    super.configure(config);
+
+    config.put(TYPE, "theme");
+  }
+
   @Override
   protected InputStream getInputStream() {
-    return getClass().getResourceAsStream("default.theme.package.json");
+    return getClass().getResourceAsStream("default.package.json");
   }
 
   public static DefaultSenchaThemePackageConfigurer getInstance() {
