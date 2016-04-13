b0d596421dc5a28eefa8944d36746bde0e67c5e4
Frank Wienberg <frank.wienberg@coremedia.com>
2016-04-13 10:44:13 +0200

    Added test that non-static const fields are added to the Ext class prototype...
    ...and not assigned in generated constructor code.

---
+++ jangaroo/jangaroo-compiler/src/test/resources/expected/package2/TestPrototypeConstants.js
@@ -0,0 +1,16 @@
+Ext.define("AS3.package2.TestPrototypeConstants", function(TestPrototypeConstants) {/*package package2 {
+
+public class TestPrototypeConstants {
+
+  public const foo:String = "FOO";
+  public const bar:Object =*/function bar_(){this.bar=( {});}/*;
+
+}*/function TestPrototypeConstants$() {bar_.call(this);}/*
+}
+
+============================================== Jangaroo part ==============================================*/
+    return {
+      foo: "FOO",
+      constructor: TestPrototypeConstants$
+    };
+});
