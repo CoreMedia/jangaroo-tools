19cd3110216fa04125525be36a25ff5b68a84d37
Frank Wienberg <frank.wienberg@coremedia.com>
2016-07-04 12:04:50 +0200

    Fix: Resolve types in relations to always use fully-qualified name.
    Keeping non-qualified type names leads to semantically equal
    method signatures not being treated as equal, which again leads
    to wrong compile errors, claiming that interface methods are not
    implemented.

--- jangaroo/jangaroo-compiler/src/test/resources/expected/package1/mxml/InterfaceImplementingMxmlClass.js
+++ jangaroo/jangaroo-compiler/src/test/resources/expected/package1/mxml/InterfaceImplementingMxmlClass.js
@@ -1,11 +1,22 @@
 Ext.define("package1.mxml.InterfaceImplementingMxmlClass", function(InterfaceImplementingMxmlClass) {/*package package1.mxml{
 import package1.*;
 import package1.mxml.YetAnotherInterface;
+import net.jangaroo.ext.Exml;
 public class InterfaceImplementingMxmlClass extends ConfigClass implements package1.mxml.YetAnotherInterface{
 
-    public native function createInstance(o:SimpleInterface):package1.mxml.SimpleClass;public*/function InterfaceImplementingMxmlClass$(config/*:InterfaceImplementingMxmlClass=null*/){package1.ConfigClass.prototype.constructor.call(this);if(arguments.length<=0)config=null;
+    public*/function InterfaceImplementingMxmlClass$(config/*:InterfaceImplementingMxmlClass = null*/){if(arguments.length<=0)config=null;
     var config_$1/*:InterfaceImplementingMxmlClass*/ =AS3.cast(InterfaceImplementingMxmlClass,{});
-}/*}}
+    var defaults_$1/*:InterfaceImplementingMxmlClass*/ ={};
+    config= net.jangaroo.ext.Exml.apply(defaults_$1,config); net.jangaroo.ext.Exml.apply(config_$1,config);package1.ConfigClass.prototype.constructor.call(this,config_$1);}/*
+
+    public native function createInstance(o:SimpleInterface):package1.mxml.SimpleClass;
+
+    [Bindable]
+    public native function get someProperty():String;
+
+    /** @private * /
+    [Bindable]
+    public native function set someProperty(value:String):void;}}
 
 ============================================== Jangaroo part ==============================================*/
     return {
@@ -15,6 +26,7 @@ public class InterfaceImplementingMxmlClass extends ConfigClass implements packa
       requires: [
         "package1.ConfigClass",
         "package1.mxml.YetAnotherInterface"
-      ]
+      ],
+      uses: ["net.jangaroo.ext.Exml"]
     };
 });
