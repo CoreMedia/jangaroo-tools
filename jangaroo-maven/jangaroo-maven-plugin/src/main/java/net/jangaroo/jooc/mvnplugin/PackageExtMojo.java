7b8b52f312485d6ad6580ef8856d6c5d67b83dd9
Andreas Gawecki <andreas.gawecki@coremedia.com>
2016-09-09 11:21:01 +0200

    compress themes neptune and triton js sources as well

--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/PackageExtMojo.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/PackageExtMojo.java
@@ -13,6 +13,8 @@ import org.apache.maven.project.MavenProjectHelper;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Packages the Ext JS framework with source maps
@@ -28,13 +30,15 @@ public class PackageExtMojo extends AbstractMojo {
   @Parameter(defaultValue = "${project.build.directory}/unzip", readonly = true)
   private File extFrameworkDir;
 
-
-
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-
     getLog().info("Execute sencha package Ext mojo");
+    packageExtAll();
+    packageTheme("theme-neptune");
+    packageTheme("theme-triton");
+  }
 
+  private void packageExtAll() throws MojoExecutionException {
     File classicPackageDir = new File(extFrameworkDir, "classic/classic");
     File fileList = new File(classicPackageDir, FILE_LIST_FILE_NAME);
     File extBuildDirectory = new File(extFrameworkDir, "build");
@@ -49,10 +53,52 @@ public class PackageExtMojo extends AbstractMojo {
     senchaCmdExecutor.execute();
 
     getLog().info(String.format("Compressing Ext to %s", extPackageFile.getPath()));
+    compressJsFiles(fileList, extPackageFile);
+  }
+
+  /**
+   * Renames the overrides directory to overrides-src and concatenates all files therin to overrides/<theme>.js
+   */
+  private void packageTheme(String theme) throws MojoExecutionException {
+    File themeDir = new File(extFrameworkDir, "classic/" + theme);
+    File overridesDir = new File(themeDir, "overrides");
+    File overridesSrcDir = new File(themeDir, "overrides-src");
+    File packageFile = new File(overridesDir, theme + "-overrides.js");
+    if (!overridesDir.renameTo(overridesSrcDir)) {
+      throw new MojoExecutionException("cannot rename " + overridesDir + " to " + overridesSrcDir);
+    }
+    if (!overridesDir.mkdir()) {
+      throw new MojoExecutionException("cannot create directory " + overridesDir);
+    }
+    List<File> sources = new ArrayList<>();
+    scanSources(overridesSrcDir, sources);
+    getLog().info(String.format("Compressing %s to %s", overridesSrcDir.getPath(), packageFile.getPath()));
     try {
-      new CompressorImpl().compressFileList(fileList, extPackageFile);
+      new CompressorImpl().compress(sources, packageFile);
     } catch (IOException e) {
-      throw new MojoExecutionException("exception while packaging JavaScript sources", e);
+      throw new MojoExecutionException("Exception while packaging JavaScript sources", e);
     }
   }
+
+  private void compressJsFiles(File fileList, File packageFile) throws MojoExecutionException {
+    try {
+      new CompressorImpl().compressFileList(fileList, packageFile);
+    } catch (IOException e) {
+      throw new MojoExecutionException("Exception while packaging JavaScript sources", e);
+    }
+  }
+
+  private void scanSources(File dir, List<File> sources) {
+    File[] children = dir.listFiles();
+    if (children != null) {
+      for (File child : children) {
+        if (child.isDirectory()) {
+          scanSources(child, sources);
+        } else if (child.getName().endsWith(".js")) {
+          sources.add(child);
+        }
+      }
+    }
+  }
+
 }
