b4cd539473f23de7de56e37e17db07200e8500ca
Frank Wienberg <frank.wienberg@coremedia.com>
2017-01-23 19:53:51 +0100

    Oops, generated expected code with wrong compiler version

--- jangaroo/jangaroo-compiler/src/test/resources/expected/package1/mxml/WhitespaceAroundBindingExpression.js
+++ jangaroo/jangaroo-compiler/src/test/resources/expected/package1/mxml/WhitespaceAroundBindingExpression.js
@@ -1,24 +1,17 @@
 Ext.define("package1.mxml.WhitespaceAroundBindingExpression", function(WhitespaceAroundBindingExpression) {/*package package1.mxml{
 import ext.*;
-import net.jangaroo.ext.Exml;
 public class WhitespaceAroundBindingExpression extends Panel{
 
-    import ext.layout.ContainerLayout;override protected*/function initConfig(_config/*:Object*/)/*:void*/{
-    var config/*:WhitespaceAroundBindingExpression*/ =AS3.cast(WhitespaceAroundBindingExpression,_config);
+    import ext.layout.ContainerLayout;public*/function WhitespaceAroundBindingExpression$(config/*:WhitespaceAroundBindingExpression=null*/){ext.Panel.prototype.constructor.call(this);if(arguments.length<=0)config=null;
     var config_$1/*:WhitespaceAroundBindingExpression*/ =AS3.cast(WhitespaceAroundBindingExpression,{});
-    var defaults_$1/*:WhitespaceAroundBindingExpression*/ =AS3.cast(WhitespaceAroundBindingExpression,{});
-    config= net.jangaroo.ext.Exml.apply(defaults_$1,config);
-    config_$1["layout"] = new ext.layout.ContainerLayout(); net.jangaroo.ext.Exml.apply(config_$1,config);ext.Panel.prototype.initConfig.call(this,config_$1);
+    config_$1["layout"] = new ext.layout.ContainerLayout();
 }/*}}
 
 ============================================== Jangaroo part ==============================================*/
     return {
       extend: "ext.Panel",
-      initConfig: initConfig,
+      constructor: WhitespaceAroundBindingExpression$,
       requires: ["ext.Panel"],
-      uses: [
-        "ext.layout.ContainerLayout",
-        "net.jangaroo.ext.Exml"
-      ]
+      uses: ["ext.layout.ContainerLayout"]
     };
 });
