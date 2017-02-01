5dd730f362a34e8aaf305ef42645ff9cd5155932
Frank Wienberg <frank.wienberg@coremedia.com>
2017-02-01 11:28:46 +0100

    Fix: Do not resolve private members nor methods as properties
    When resolving a property declaration from an MXML attribute or property
    node, ignore declarations of private members and methods. These cannot
    be set from MXML and may hide events declared by [Event] annotations.

--- jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/mxml/ast/MxmlToModelParser.java
+++ jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/mxml/ast/MxmlToModelParser.java
@@ -571,7 +571,7 @@ final class MxmlToModelParser {
     }
     if (propertyModel == null) {
       TypedIdeDeclaration memberModel = classModel.getMemberDeclaration(propertyName);
-      if (memberModel != null) { // TODO: && memberModel.isWritable()) {
+      if (memberModel != null && !memberModel.isPrivate() && !memberModel.isMethod()) {
         propertyModel = memberModel;
       }
     }
