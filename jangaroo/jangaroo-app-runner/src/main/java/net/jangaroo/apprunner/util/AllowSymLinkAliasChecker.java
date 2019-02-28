38823746b672ad4cb0c5e52f8f5b2e44aabd0a44
fwienber <frank.wienberg@coremedia.com>
2019-02-28 12:09:03 +0100

    For JOO-143: Reformat copied code of AllowSymLinkAliasChecker

--- jangaroo/jangaroo-app-runner/src/main/java/net/jangaroo/apprunner/util/AllowSymLinkAliasChecker.java
+++ jangaroo/jangaroo-app-runner/src/main/java/net/jangaroo/apprunner/util/AllowSymLinkAliasChecker.java
@@ -18,15 +18,15 @@
 
 package net.jangaroo.apprunner.util;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandler.AliasCheck;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.resource.PathResource;
 import org.eclipse.jetty.util.resource.Resource;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 /* ------------------------------------------------------------ */
@@ -38,62 +38,55 @@ import org.eclipse.jetty.util.resource.Resource;
  * Java {@link Files#readSymbolicLink(Path)} and {@link Path#toRealPath(java.nio.file.LinkOption...)}
  * APIs to check if a file is aliased with symbolic links.</p>
  */
-public class AllowSymLinkAliasChecker implements AliasCheck
-{
-    private static final Logger LOG = Log.getLogger(AllowSymLinkAliasChecker.class);
-    
-    @Override
-    public boolean check(String uri, Resource resource)
-    {
-        // Only support PathResource alias checking
-        if (!(resource instanceof PathResource))
-            return false;
-        
-        PathResource pathResource = (PathResource) resource;
+public class AllowSymLinkAliasChecker implements AliasCheck {
+  private static final Logger LOG = Log.getLogger(AllowSymLinkAliasChecker.class);
 
-        try
-        {
-            Path path = pathResource.getPath();
-            Path alias = pathResource.getAliasPath();
-
-            if (path.equals(alias))
-                return false; // Unknown why this is an alias
-
-            if (hasSymbolicLink(path) && Files.isSameFile(path, alias))
-            {
-                if (LOG.isDebugEnabled())
-                    LOG.debug("Allow symlink {} --> {}", resource, pathResource.getAliasPath());
-                return true;
-            }
-        }
-        catch (Exception e)
-        {
-            LOG.ignore(e);
-        }
-        
-        return false;
+  @Override
+  public boolean check(String uri, Resource resource) {
+    // Only support PathResource alias checking
+    if (!(resource instanceof PathResource)) {
+      return false;
     }
 
-    private boolean hasSymbolicLink(Path path)
-    {
-        // Is file itself a symlink?
-        if (Files.isSymbolicLink(path))
-        {
-            return true;
-        }
+    PathResource pathResource = (PathResource) resource;
 
-        // Lets try each path segment
-        Path base = path.getRoot();
-        for (Path segment : path)
-        {
-            base = base.resolve(segment);
-            if (Files.isSymbolicLink(base))
-            {
-                return true;
-            }
-        }
+    try {
+      Path path = pathResource.getPath();
+      Path alias = pathResource.getAliasPath();
 
-        return false;
+      if (path.equals(alias)) {
+        return false; // Unknown why this is an alias
+      }
+
+      if (hasSymbolicLink(path) && Files.isSameFile(path, alias)) {
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("Allow symlink {} --> {}", resource, pathResource.getAliasPath());
+        }
+        return true;
+      }
+    } catch (Exception e) {
+      LOG.ignore(e);
     }
+
+    return false;
+  }
+
+  private boolean hasSymbolicLink(Path path) {
+    // Is file itself a symlink?
+    if (Files.isSymbolicLink(path)) {
+      return true;
+    }
+
+    // Lets try each path segment
+    Path base = path.getRoot();
+    for (Path segment : path) {
+      base = base.resolve(segment);
+      if (Files.isSymbolicLink(base)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 
 }
