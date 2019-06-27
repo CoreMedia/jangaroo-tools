4696f8b7cfdc7e9caf118a829901404f0c6da123
Frank Wienberg <frank.wienberg@coremedia.com>
2019-06-27 18:03:12 +0200

    Fix JOO-146: Allow non-public domains and self-signed certificates
    Verify host name, but suppress public domain suffix check.
    No longer trust *all* certificates, but allow self-signed
    certificates.
    This improves security at least somewhat, while supporting most
    developer use cases.
    Note that the goal jangaroo:run which uses this strategy is meant
    as a development tool, not to be used in production!
    During development, you will want to connect to servers from
    internal domains with self-
    signed certificates, so allowing this makes sense.

--- jangaroo/jangaroo-app-runner/src/main/java/net/jangaroo/apprunner/proxy/HttpClientUtil.java
+++ jangaroo/jangaroo-app-runner/src/main/java/net/jangaroo/apprunner/proxy/HttpClientUtil.java
@@ -2,39 +2,23 @@ package net.jangaroo.apprunner.proxy;
 
 import org.apache.http.conn.ssl.DefaultHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.security.KeyManagementException;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 
 class HttpClientUtil {
-  private static final X509TrustManager TRUST_MANAGER = new X509TrustManager() {
-    @Override
-    public void checkClientTrusted(X509Certificate[] chain, String authType) {
-    }
-
-    @Override
-    public void checkServerTrusted(X509Certificate[] chain, String authType) {
-    }
-
-    @Override
-    public X509Certificate[] getAcceptedIssuers() {
-      return null;
-    }
-  };
 
   static SSLConnectionSocketFactory createSSLSocketFactory() {
-    SSLContext ctx;
     try {
-      ctx = SSLContext.getInstance("TLS");
-      ctx.init(null, new TrustManager[]{TRUST_MANAGER}, null);
-    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+      SSLContextBuilder sslContextBuilder = SSLContextBuilder.create()
+              .loadTrustMaterial(null, new TrustSelfSignedStrategy());
+      return new SSLConnectionSocketFactory(sslContextBuilder.build(), new DefaultHostnameVerifier());
+    } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
       throw new IllegalStateException(e);
     }
 
-    return new SSLConnectionSocketFactory(ctx, new DefaultHostnameVerifier());
   }
 }
