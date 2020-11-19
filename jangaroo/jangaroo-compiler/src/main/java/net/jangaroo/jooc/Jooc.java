8f38eed9e8a93fb292c2a8cf5321942e055b2fbd
fwienber <frank.wienberg@coremedia.com>
2020-11-19 10:47:27 +0100

    [TS/Maven] Fix: Do _not_ use .d.ts for [Native] joo-api
    In "generateActionScriptApi" mode, even when migrating to
    TypeScript, the nativeSuffix must _not_ change to .d.ts.

--- jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/Jooc.java
+++ jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/Jooc.java
@@ -336,7 +336,7 @@ public class Jooc extends JangarooParser implements net.jangaroo.jooc.api.Jooc {
     } else {
       File outputDirectory = generateActionScriptApi ? config.getApiOutputDirectory() : config.getOutputDirectory();
       final String suffix = generateActionScriptApi ? AS_SUFFIX : config.isMigrateToTypeScript() ? ".ts" : OUTPUT_FILE_SUFFIX;
-      final String nativeSuffix = config.isMigrateToTypeScript() ? ".d.ts" : null;
+      final String nativeSuffix = !generateActionScriptApi && config.isMigrateToTypeScript() ? ".d.ts" : null;
       codeSinkFactory = new SingleFileCompilationUnitSinkFactory(config, outputDirectory, generateActionScriptApi, suffix, nativeSuffix, this, this);
     }
     return codeSinkFactory;
