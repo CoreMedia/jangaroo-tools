939c967dd792c5d80b0c5ba2468efc95d8ca842e
Frank Wienberg <frank.wienberg@coremedia.com>
2021-02-15 18:53:53 +0100

    Fix stale Resource() references in PropertiesTest.properties
    While for JS code generation, Resource() references in properties
    files to non-resolvable bundles are not detected at compile time,
    in TS mode, the error is detected and thus would lead to a test
    failure.

--- jangaroo/jangaroo-compiler/src/test/resources/expectedProperties/en/testPackage/PropertiesTest_properties.js
+++ jangaroo/jangaroo-compiler/src/test/resources/expectedProperties/en/testPackage/PropertiesTest_properties.js
@@ -5,15 +5,15 @@
 Ext.define("testPackage.PropertiesTest_properties", {
 
   requires: [
-    "net.jangaroo.icons.SomeBundle_properties",
-    "net.jangaroo.SomeOtherBundle_properties"
+    "testPackage.MoreIcons_properties",
+    "testPackage.Icons_properties"
   ],
   "key": "The disk \"{1}\" contains {0}.",
   "keyWith\"+\"quotes": "gotcha!"
 }, function() {
-  this.prototype["key2"] =  net.jangaroo.icons.SomeBundle_properties.INSTANCE.someKey;
-  this.prototype["key3"] =  net.jangaroo.SomeOtherBundle_properties.INSTANCE.someOtherKey;
-  this.prototype["keyWith\"+\"quotesAndReference"] =  net.jangaroo.SomeOtherBundle_properties.INSTANCE.someOtherKey;
+  this.prototype["key2"] =  testPackage.Icons_properties.INSTANCE.someKey;
+  this.prototype["key3"] =  testPackage.MoreIcons_properties.INSTANCE.someOtherKey;
+  this.prototype["keyWith\"+\"quotesAndReference"] =  testPackage.MoreIcons_properties.INSTANCE.someOtherKey;
 
   testPackage.PropertiesTest_properties.INSTANCE = new testPackage.PropertiesTest_properties();
 });
