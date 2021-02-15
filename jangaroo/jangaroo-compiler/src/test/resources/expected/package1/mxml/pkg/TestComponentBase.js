94ac8b25e35dac2f1bc2acf3baebde7fc8fa84bd
Frank Wienberg <frank.wienberg@coremedia.com>
2021-02-15 19:00:03 +0100

    Also test compilation result of TestComponentBase class
    While this is not necessary for compilation to JS, it causes
    trouble when targeting TS and is cleaner, anyway.

---
+++ jangaroo/jangaroo-compiler/src/test/resources/expected/package1/mxml/pkg/TestComponentBase.js
@@ -0,0 +1,47 @@
+/*package package1.mxml.pkg {*/
+
+
+Ext.define("package1.mxml.pkg.TestComponentBase", function(TestComponentBase) {/*public class TestComponentBase implements TestInterface {
+
+  public static const DEFAULT:String = "_DEFAULT_";
+
+  [ExtConfig]
+  [Bindable]
+  /**
+   * @private
+   * /
+  public native function set store(value:*):void;
+
+  public var emptyText:String;
+  public var letters:Array;
+
+  private var property_1:String;
+  private var property_2:int;
+
+  public*/ function TestComponentBase$(config/*:TestComponent = null*/) {if(arguments.length<=0)config=null;
+    this.property_1$00xv = AS3.setBindable(config,"property_1" , "_HI");
+    this.property_2$00xv = config.property_2 || 0;
+  }/*
+
+  private var component:Object;
+
+  public*/ function init(component/*:Object*/)/*:void*/ {
+    this.component$00xv = component;
+  }/*
+}
+}
+
+============================================== Jangaroo part ==============================================*/
+    return {
+      mixins: ["package1.mxml.pkg.TestInterface"],
+      emptyText: null,
+      letters: null,
+      property_1$00xv: null,
+      property_2$00xv: 0,
+      constructor: TestComponentBase$,
+      component$00xv: null,
+      init: init,
+      statics: {DEFAULT: "_DEFAULT_"},
+      requires: ["package1.mxml.pkg.TestInterface"]
+    };
+});
