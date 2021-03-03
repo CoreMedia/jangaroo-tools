63d3ee9e1da20293ddfa7edf4a61b7aafd943231
Frank Wienberg <frank.wienberg@coremedia.com>
2021-03-03 19:20:58 +0100

    Remove unused import

--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/sencha/SenchaUtils.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/sencha/SenchaUtils.java
@@ -1,6 +1,5 @@
 package net.jangaroo.jooc.mvnplugin.sencha;
 
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
