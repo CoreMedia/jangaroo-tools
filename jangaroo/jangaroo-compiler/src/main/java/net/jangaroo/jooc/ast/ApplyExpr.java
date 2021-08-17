ab39d21d2035224e12381d45b7c7722db5369d1f
Alexander Holtkamp <alexander.holtkamp@coremedia.com>
2021-08-17 09:49:23 +0200

    added "Error" to COERCE_FUNCTION_NAMES to avoid conversion to AS3.cast

--- jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/ast/ApplyExpr.java
+++ jangaroo/jangaroo-compiler/src/main/java/net/jangaroo/jooc/ast/ApplyExpr.java
@@ -1,15 +1,15 @@
 /*
  * Copyright 2008 CoreMedia AG
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an "AS
- * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
- * express or implied. See the License for the specific language 
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
 
@@ -45,7 +45,7 @@ public class ApplyExpr extends Expr {
   private ParenthesizedExpr<CommaSeparatedList<Expr>> args;
 
   private boolean insideNewExpr = false;
-  private static final Set<String> COERCE_FUNCTION_NAMES = new HashSet<String>(Arrays.asList("Number", "String", "Boolean", "int", "uint", "Date", "Object", "Array", "RegExp", "XML"));
+  private static final Set<String> COERCE_FUNCTION_NAMES = new HashSet<String>(Arrays.asList("Number", "String", "Boolean", "int", "uint", "Date", "Object", "Array", "RegExp", "XML", "Error"));
   private Scope scope;
   private Map<Expr, ClassDeclaration> argToPropertiesClass = new HashMap<>();
 
