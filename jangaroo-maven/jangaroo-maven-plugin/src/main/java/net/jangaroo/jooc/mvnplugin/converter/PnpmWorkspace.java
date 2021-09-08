ffa4cada599165d6ef617ee40801dd8665454d55
Frank Wienberg <frank.wienberg@coremedia.com>
2021-09-08 18:33:45 +0200

    Add no-arg constructor to PnpmWorkspace
    Needed by Jackson's ObjectMapper.

--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/converter/PnpmWorkspace.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/converter/PnpmWorkspace.java
@@ -1,13 +1,21 @@
 package net.jangaroo.jooc.mvnplugin.converter;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class PnpmWorkspace {
 
+  @JsonProperty("packages")
   private List<String> packages;
 
   public PnpmWorkspace(List<String> packages) {
     this.packages = packages;
+  }
+
+  // No-arg constructor to be used by Jackson's ObjectMapper
+  @SuppressWarnings("unused")
+  public PnpmWorkspace() {
   }
 
   public List<String> getPackages() {
