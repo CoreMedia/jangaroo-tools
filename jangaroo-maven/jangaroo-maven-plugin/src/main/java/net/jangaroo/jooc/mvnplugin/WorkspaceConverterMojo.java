b46a1663036c4691806eaf218edb7ac2daa85e20
Alexander Holtkamp <alexander.holtkamp@coremedia.com>
2021-10-11 19:18:15 +0200

    renamed studio-client extension point to studio-client.main
    also switched special naming from "-dynamic" (new default) to "-static" (old default)

--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/WorkspaceConverterMojo.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/WorkspaceConverterMojo.java
@@ -667,10 +667,10 @@ public class WorkspaceConverterMojo extends AbstractMojo {
   }
 
   private String renameLegacyProjectExtensionPoint(String projectExtensionPointName) {
-    if ("studio".equals(projectExtensionPointName)) {
-      return "studio-client";
-    } else if ("studio-dynamic".equals(projectExtensionPointName)) {
-      return "studio-client-dynamic";
+    if ("studio".equals(projectExtensionPointName) || "studio-client".equals(projectExtensionPointName)) {
+      return "studio-client.main-static";
+    } else if ("studio-dynamic".equals(projectExtensionPointName) || "studio-client-dynamic".equals(projectExtensionPointName)) {
+      return "studio-client.main";
     }
     return projectExtensionPointName;
   }
