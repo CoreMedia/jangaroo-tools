84ce28d4f390bb655330e0493a681372c5eea50e
Frank Wienberg <frank.wienberg@coremedia.com>
2022-01-10 13:16:58 +0100

    Change: Always use OS-independent new-line '\n'
    We want the result to be the same, no matter which OS
    runs the conversion tool. Not sure whether this is the
    case for other generated files, but as a start, the
    generated package.json and jangaroo.config.js now always
    use '\n' for line endings.

--- jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/converter/PackageJsonPrettyPrinter.java
+++ jangaroo-maven/jangaroo-maven-plugin/src/main/java/net/jangaroo/jooc/mvnplugin/converter/PackageJsonPrettyPrinter.java
@@ -5,8 +5,13 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 
 public class PackageJsonPrettyPrinter extends DefaultPrettyPrinter {
 
+  private static final DefaultIndenter DEFAULT_INDENTER = new DefaultIndenter("  ", "\n");
+
   public PackageJsonPrettyPrinter() {
-    super(new DefaultPrettyPrinter().withArrayIndenter(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE).withSpacesInObjectEntries());
+    super(new DefaultPrettyPrinter()
+            .withArrayIndenter(DEFAULT_INDENTER)
+            .withObjectIndenter(DEFAULT_INDENTER)
+            .withSpacesInObjectEntries());
     // we want a space only after the colon:
     _objectFieldValueSeparatorWithSpaces = _separators.getObjectFieldValueSeparator() + " ";
   }
